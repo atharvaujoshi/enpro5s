4905dcbc2d24f5a78c51c6b24e198ef6d0deebb2
Atharva Joshi <atharvaujoshi@gmail.com>
2026-02-15 18:25:45 +0530

    chore: Enable SWC by removing .babelrc and configure allowedDevOrigins

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['localhost'],
   },
+  experimental: {
+    allowedDevOrigins: ['192.168.137.1', 'localhost:3000', 'localhost:3001', 'localhost:3002']
+  }
 }
 
 module.exports = nextConfig
